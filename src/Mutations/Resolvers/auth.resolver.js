ba17bf8442106eb793bef47a79bb7366f7ce72be
Vinicius Rangel <vinicius.rangel@ad4pixels.com.br>
2018-09-11 19:34:54 -0300

    Ajuste no serviço de idade

--- src/Mutations/Resolvers/auth.resolver.js
+++ src/Mutations/Resolvers/auth.resolver.js
@@ -1,9 +1,17 @@
 import {User, Config} from '../../db/mysql';
 import {WrongPasswordError, WrongCredentials, EmailExist} from '../errorHandler';
+import sequelize from 'sequelize';
 
 const fetch = {
     login: async function (args){
-        const result = await User.findOne({where:{email:args.email}});
+        const result = await User.findOne({
+            attributes: {
+                include:[
+                    [sequelize.fn('timestampdiff', sequelize.literal('YEAR'), sequelize.col('date_birthday'), sequelize.fn('NOW')),'age']
+                ]
+            },
+            where:{email:args.email}
+        });
         
         if(result){
             if(args.password == result.password){
